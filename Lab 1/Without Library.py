ffa6a679f2fd5a053adcf2f990239c7a64998efb
Yash2108 <1999yashjain@gmail.com>
2021-08-11 20:38:12 +0530

    Finished Labs 1 and 2.

---
+++ Lab 1/Without Library.py
@@ -0,0 +1,81 @@
+from string import punctuation
+
+text = "Natural language processing (NLP) is a field " + \
+       "of computer science, artificial intelligence " + \
+       "and computational linguistics concerned with " + \
+       "the interactions between computers and human " + \
+       "(natural) languages, and, in particular, " + \
+       "concerned with programming computers to " + \
+       "fruitfully process large natural language " + \
+       "corpora. Challenges in natural language " + \
+       "processing frequently involve natural " + \
+       "language understanding, natural language" + \
+       "generation frequently from formal, machine" + \
+       "-readable logical forms), connecting language " + \
+       "and machine perception, managing human-" + \
+       "computer dialog systems, or some combination " + \
+       "thereof."
+
+sentences=[i if i.endswith('.')  else i+'.' for i in text.split('. ') ]
+words=text.split()
+sentences_wo_punc=[]
+words_wo_punc=[]
+tokens={}
+tokens_wo_punc={}
+
+for i in sentences:
+    x=''
+    for j in i:
+        if j not in punctuation:
+            x+=j
+    if x!='':
+        sentences_wo_punc.append(x)
+
+for i in words:
+    x=''
+    for j in i:
+        if j not in punctuation:
+            x+=j
+    if x!='':
+        words_wo_punc.append(x)
+
+for i in words:
+    if i.lower() not in tokens:
+        tokens[i.lower()]=1
+    else:
+        tokens[i.lower()]+=1
+        
+for i in words_wo_punc:
+    if i.lower() not in tokens_wo_punc:
+        tokens_wo_punc[i.lower()]=1
+    else:
+        tokens_wo_punc[i.lower()]+=1
+
+print("With Punctuation: ", len(tokens))
+for i in tokens:
+    print("{}:\t{}".format(i, tokens[i]))
+
+print('\n----------------------------')
+print("\nWithout Punctuation: ", len(tokens_wo_punc))
+
+for i in tokens_wo_punc:
+    print("{}:\t{}".format(i, tokens_wo_punc[i]))
+
+chara={}
+chara_wo_punc={}
+
+for i in text:
+    if i not in punctuation:
+        if i.lower() not in chara_wo_punc:
+            chara_wo_punc[i.lower()]=1
+        else:
+            chara_wo_punc[i.lower()]+=1
+    if i.lower() not in chara:
+        chara[i.lower()]=1
+    else:
+        chara[i.lower()]+=1
+print("With Punctuations: ", len(chara))
+print(chara)
+
+print("Without Punctuations: ", len(chara_wo_punc))
+print(chara_wo_punc)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# NLPLab
+# CS491 - Natural Language Processing
+
+This repo contains labwork and any other coding practice done for the course of CS491 - Natural Language Processing.
+
+Table of Contents:
+- Lab 1: Tokenizing with and without using NLTK library
+  - [Instructions](https://github.com/Yash2108/NLPLab/blob/main/Lab1/Lab%201%20Instructions.docx)
+- Lab 2: Find the % of overlap with standard list of stop words and your list of stop words
+  - [Instructions](https://github.com/Yash2108/NLPLab/blob/main/Lab%202/Lab%202%20Instructions.docx)
